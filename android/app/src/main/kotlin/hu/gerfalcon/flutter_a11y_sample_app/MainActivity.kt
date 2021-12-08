735a54d9bf49cebcb44e2822fe10f799a88ae8cb
Csongor Vogel <gerfalcon.vogel@gmail.com>
2021-12-08 16:07:05 +0100

    First commit with the starter project

---
+++ android/app/src/main/kotlin/hu/gerfalcon/flutter_a11y_sample_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package hu.gerfalcon.flutter_a11y_sample_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/page/home/details/details_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_a11y_sample_app/domain/model/santa.dart';
+
+class DetailPage extends StatefulWidget {
+  final Santa santa;
+
+  const DetailPage({
+    Key? key,
+    required this.santa,
+  }) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Hero(
+          tag: "background_${widget.santa.name}",
+          child: Container(
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            elevation: 0,
+            title: Text(widget.santa.name),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Hero(
+                  tag: "image_${widget.santa.name}",
+                  child: Image.asset(
+                    widget.santa.avatar,
+                    height: MediaQuery.of(context).size.height / 2,
+                  ),
+                ),
+                AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, widget) => Transform.translate(
+                    transformHitTests: false,
+                    offset: Offset.lerp(
+                      const Offset(0.0, 200.0),
+                      Offset.zero,
+                      _controller.value,
+                    )!,
+                    child: widget,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      widget.santa.description,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
